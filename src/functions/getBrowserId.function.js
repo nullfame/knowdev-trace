6f54486523e40f17e86c3cbea3a6fdea16dc8a51
Adam Finlayson <amf@adamfinlayson.com>
2023-12-31 14:25:11 -0600

    feat: set a browser id

--- src/functions/getBrowserId.function.js
+++ src/functions/getBrowserId.function.js
@@ -1,3 +1,6 @@
+import { v4 as uuidv4 } from "uuid";
+import { getApp, getOptions } from "../core.js";
+
 //
 //
 // Constants
@@ -13,8 +16,26 @@
 // Main
 //
 
-// eslint-disable-next-line arrow-body-style
 const getBrowserId = () => {
+  const app = getApp();
+  if (!app) return false;
+  const cookies = app.$cookies;
+  if (!cookies) return false;
+
+  const options = getOptions();
+  const { cookieName } = options;
+
+  if (cookies) {
+    // See if a cookie is present
+    const browserId = cookies.get(cookieName);
+    if (browserId) return browserId;
+    // If not, generate a new uuid
+    const newBrowserId = uuidv4();
+    // Save it as a cookie
+    app.$cookies.set(cookieName, newBrowserId);
+    return newBrowserId;
+  }
+
   return false;
 };
 
